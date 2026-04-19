f341ee7b1a6b51960c5fb43a0ee6331d46b3e124
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2026-04-19 00:00:18 -0500

    feat: implement SQS notification dispatcher for payment confirmations.

--- services/payments/src/assembly.py
+++ services/payments/src/assembly.py
@@ -9,6 +9,7 @@ from adapters.repositories.payment_repository import SQLModelPaymentRepository
 from adapters.services.notification_dispatcher import (
     HttpNotificationDispatcher,
     NoOpNotificationDispatcher,
+    SqsNotificationDispatcher,
 )
 from adapters.services.reservation_updater import (
     HttpReservationUpdater,
@@ -62,7 +63,10 @@ def get_stripe_checkout_gateway() -> StripeCheckoutGateway:
 
 
 def get_notification_dispatcher() -> NotificationDispatcher:
-    if settings.notifications_service_url:
+    mode = settings.notifications_dispatch_mode
+    if mode == "sqs":
+        return SqsNotificationDispatcher()
+    if mode == "http":
         return HttpNotificationDispatcher()
     return NoOpNotificationDispatcher()
 
